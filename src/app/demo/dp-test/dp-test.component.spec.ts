90d63e519374bb7f3922d7d7c71f6cfa1e28daed
Jan-Hendrik Oldag <jan@MacBook-135.local>
2018-04-17 07:37:23 +0200

    fixed unit test

--- src/app/demo/dp-test/dp-test.component.spec.ts
+++ src/app/demo/dp-test/dp-test.component.spec.ts
@@ -1,31 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { DayCalendarComponent} from '../../';
 import { DpTestComponent } from './dp-test.component';
 
-describe('DpTestComponent', () => {
-  let component: DpTestComponent;
-  let fixture: ComponentFixture<DpTestComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-
-      ],
-      declarations: [
-        DpTestComponent,
-        DayCalendarComponent
-      ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DpTestComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
+describe('Component: Test', () => {
+  it('should create an instance', () => {
+    const component = new DpTestComponent();
     expect(component).toBeTruthy();
   });
 });
